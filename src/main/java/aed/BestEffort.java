79d3e984fa1f3a67fa3024f2af881b804a8f4ed6
Ian Szulman <ian.g.szulman@gmail.com>
2024-11-16 18:53:02 -0300

    reordenamiento

--- src/main/java/aed/BestEffort.java
+++ src/main/java/aed/BestEffort.java
@@ -7,25 +7,11 @@ import aed.ColaPrioridad.Heap;
 import aed.ColaPrioridad.HeapEnlazado;
 
 public class BestEffort {
-    private ArrayList<Integer> ciudades; //no sé si esto es array de integers. necesita un puntero al nodo de cada ciudad, eso sí
+    private ArrayList<Integer> posCiudadesEnHeap; //no sé si esto es array de integers. necesita un puntero al nodo de cada ciudad, eso sí
     private ArrayList<Integer> ciudadesMayorGanancia;
     private ArrayList<Integer> ciudadesMayorPerdida;
     private Integer gananciasTotales;
     private Integer trasladosTotales;
-
-
-    public BestEffort(int cantCiudades, Traslado[] traslados){
-        //vamos a decir que cantCiudades == C, y traslados == T, ya que esta función establece las ciudades y las estructuras de los traslados del sistema
-        ArrayList<Integer> citys = new ArrayList<Integer>();
-        Integer i = 0;
-        for (i=0; i<cantCiudades; i++) //O(C)
-        {
-            citys.add(i); //O(1)
-        }
-        ciudades=citys; //O(1)
-        ciudadesMayorGanancia=citys; //O(1)
-        ciudadesMayorPerdida=citys; //O(1)
-    }
 
     private Comparator<Traslado> comparadorMasRedituable =(t1, t2) ->{
         if (t1.gananciaNeta != t2.gananciaNeta){
@@ -41,6 +27,36 @@ public class BestEffort {
     
     private HeapEnlazado<Traslado> heapRedituable = new HeapEnlazado<>(comparadorMasRedituable);
     private HeapEnlazado<Traslado> heapAntiguo = new HeapEnlazado<>(comparadorMasAntiguo);
+
+    private Comparator<Ciudades> comparadorSuperavit = (t1, t2) -> {
+        return Integer.compare(t2.superavit, t1.superavit); 
+    };
+
+    private Heap<Ciudades> heapCiudades = new Heap<>(comparadorSuperavit); 
+
+    private void procesarDespacho(Traslado traslado){
+
+        actualizarCiudad(traslado.origen, traslado.gananciaNeta);
+
+        actualizarCiudad(traslado.destino, -traslado.gananciaNeta);
+    }
+
+    private void actualizarCiudad(int idCiudad, int gananciaNeta){
+        
+    }
+
+    public BestEffort(int cantCiudades, Traslado[] traslados){
+        //vamos a decir que cantCiudades == C, y traslados == T, ya que esta función establece las ciudades y las estructuras de los traslados del sistema
+        ArrayList<Integer> citys = new ArrayList<Integer>();
+        Integer i = 0;
+        for (i=0; i<cantCiudades; i++) //O(C)
+        {
+            citys.add(i); //O(1)
+        }
+        posCiudadesEnHeap=citys; //O(1)
+        ciudadesMayorGanancia=citys; //O(1)
+        ciudadesMayorPerdida=citys; //O(1)
+    }
 
     public void registrarTraslados(Traslado[] traslados){
 
@@ -81,26 +97,9 @@ public class BestEffort {
         return despachados;
     }
 
-    private Comparator<Ciudades> comparadorSuperavit = (t1, t2) -> {
-        return Integer.compare(t2.superavit, t1.superavit); 
-    };
-
-    private Heap<Ciudades> heapSuperavit = new Heap<>(comparadorSuperavit); 
-
-    private void procesarDespacho(Traslado traslado){
-
-        actualizarCiudad(traslado.origen, traslado.gananciaNeta);
-
-        actualizarCiudad(traslado.destino, -traslado.gananciaNeta);
-    }
-
-    private void actualizarCiudad(int idCiudad, int gananciaNeta){
-        
-    }
-
     public int ciudadConMayorSuperavit(){
         // Implementar
-        return 0;
+        return 0; //O(1)
     }
 
     public ArrayList<Integer> ciudadesConMayorGanancia(){
@@ -112,9 +111,9 @@ public class BestEffort {
     }
 
     public int gananciaPromedioPorTraslado(){
-        if (trasladosTotales==0)
+        if (trasladosTotales==0) //O(1)
         {
-            return 0;
+            return 0; //O(1)
         }
         else
         {
