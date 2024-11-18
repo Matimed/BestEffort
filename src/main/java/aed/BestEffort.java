8b09904e4c6f0dfefae17546b70017e0eadba0d4
Ian Szulman <ian.g.szulman@gmail.com>
2024-11-17 21:49:08 -0300

    algunos comentarios

--- src/main/java/aed/BestEffort.java
+++ src/main/java/aed/BestEffort.java
@@ -15,60 +15,93 @@ public class BestEffort {
     private int gananciasTotales;
     private int despachosTotales;
 
-
+    /**O(|C|+|T|)
+     * Notar que, en esta función, CantCiudades = C, y traslados = T 
+    */
     public BestEffort(int cantCiudades, Traslado[] traslados){       
+        //O(1)
         this.gananciasTotales = 0;
         this.despachosTotales = 0;
         this.cMayorGanancia = new ArrayList<Integer>();
         this.cMayorPerdida = new ArrayList<Integer>();
-        
         Ciudad[] ciudades = new Ciudad[cantCiudades];
+        
+        //O(|C|)
         for (int i = 0; i < cantCiudades; i++) {
+            //O(1)
             Ciudad ciudad = new Ciudad(i);
             this.cMayorGanancia.add(i);
             this.cMayorPerdida.add(i);
             ciudades[i] = ciudad;
         }
+
+        //O(|C|)
         this.ciudades = cSuperavit.inicialiar(ciudades);
-       
-       this.tAntiguedad.inicialiar(this.tGanancias, traslados);
+        
+        //O(|T|)
+        this.tAntiguedad.inicialiar(this.tGanancias, traslados);
     }
 
+    /*O(|traslados|*log|T|)
+    */
     public void registrarTraslados(Traslado[] traslados){
-        for (Traslado t : traslados) { tAntiguedad.apilar(t); }
+        //O(|traslados|)
+        for (Traslado t : traslados) 
+        { 
+            //O(log|T|)
+            tAntiguedad.apilar(t); 
+        }
     }
 
+    /*O(n*(log|C|+log|T|))
+    */
     public int[] despacharMasRedituables(int n){ return despacharDesde(n, this.tGanancias); }
 
+    /*O(n*(log|C|+log|T|))
+    */
     public int[] despacharMasAntiguos(int n){ return this.despacharDesde(n, this.tAntiguedad); }
     
+    /*O(1)
+    */
     public int ciudadConMayorSuperavit() { return cSuperavit.consultarMax().id; }
 
+    /*O(1)
+    */
     public ArrayList<Integer> ciudadesConMayorGanancia() { return this.cMayorGanancia;} 
 
+    /*O(1)
+    */
     public ArrayList<Integer> ciudadesConMayorPerdida() {return this.cMayorPerdida;}
-
+    
+    /*O(1)
+    */
     public int gananciaPromedioPorTraslado(){
         if (despachosTotales==0) {return 0; }
-        
-        return (int) Math.floor(gananciasTotales/despachosTotales);//dado que esta instrucción solo obtiene la parte entera de una división de enteros, es O(1)
-    }   
-
+        return (int) Math.floor(gananciasTotales/despachosTotales); }   
+    
+    /*O(n*(log|C|+log|T|))
+    */ 
     private int[] despacharDesde(int n, HeapEnlazado<Traslado> heap){
+        //O(1)
         int [] despachados = new int[n];
         int index = 0;
-
+        
+        //O(n)
         while(n > 0 && !heap.vacia()){
+            //O(log|T|)
             Traslado traslado = heap.desapilarMax();
             despachados[index ++] = traslado.id;
 
+            //O(log|C|)
             procesarDespacho(traslado);
             n--;
         }
         return despachados;
     }
  
-    private void procesarDespacho(Traslado  traslado){
+    /*O(log|C|+log|T|)
+    */
+    private void procesarDespacho(Traslado traslado){
         int mayorGanancia = this.getCiudad(this.cMayorGanancia.get(0)).ganancia;
         int mayorPerdida = this.getCiudad(this.cMayorPerdida.get(0)).perdida;
 
@@ -94,5 +127,7 @@ public class BestEffort {
         } else if (destino.perdida == mayorPerdida){ this.cMayorPerdida.add(destino.id);}
     }
     
+    /*O(1) 
+    */
     private Ciudad getCiudad(int id){  return this.ciudades.get(id).valor; }
 }
